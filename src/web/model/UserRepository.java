58156e0a3879cc00953cc02110777174c9965ca9
sunabove <sunabove@nate.com>
2018-08-04 16:09:58 +0900

    **

--- src/web/model/UserRepository.java
+++ src/web/model/UserRepository.java
@@ -1,7 +1,7 @@
 package web.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
     User findByName(String name);
 }
