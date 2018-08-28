54dd599b7d72b11d316fdc598f2aefaf82f195b1
sunabove <sunabove@nate.com>
2018-08-29 08:38:59 +0900

    **

--- src/web/model/DbFileLogList.java
+++ src/web/model/DbFileLogList.java
@@ -6,5 +6,17 @@ public class DbFileLogList extends ArrayList<DbFileLog> {
 
 	public DbFileLogList() {
 	}
+	
+	public int getDownloadCountSum() {
+		int sum = 0 ;
+		
+		for( DbFileLog item : this ) {
+			if( null != item ) { 
+				sum += null == item.downloadCount ? 0 : item.downloadCount ; 
+			}
+		}
+		
+		return sum;  
+	}
 
 }
